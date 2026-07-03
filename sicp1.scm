d2cbe416428f034d0fef8a162f877848399e4080
Kerem Atak <kerem.atak@hotmail.com>
2026-07-03 22:05:16 +0300

    e1.6

--- sicp1.scm
+++ sicp1.scm
@@ -131,3 +131,34 @@ size
 ;; With normal-order subexpression of (p) would not be evaluated as it's not necessary with arguments 0 and (p).
 
 ; 1.1.7
+
+;; e1.6
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (square x) (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (new-sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+    guess
+    (new-sqrt-iter (improve guess x) x)))
+
+(sqrt-iter 5.0 16)
+
+(new-sqrt-iter 5.0 16) ;; infinite recursion, both clauses are evaluated.
+
